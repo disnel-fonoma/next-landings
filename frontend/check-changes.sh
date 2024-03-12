ea1cc8df9b23b8a1748064ac55dde941e6f8ef76
Disnel Rodriguez <disnelrr@gmail.com>
2024-03-12 15:08:16 -0400

    Update NEXTJS_DIR in check-changes.sh

--- frontend/check-changes.sh
+++ frontend/check-changes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define the directory to check for changes
-NEXTJS_DIR="../"
+NEXTJS_DIR="."
 
 # Check if there are any changes in the Next.js directory
-if git diff --quiet HEAD^ HEAD -- $NEXTJS_DIR; then
+if git diff HEAD^ HEAD -- $NEXTJS_DIR; then
  # No changes detected, exit with 0
  exit 0
 else
